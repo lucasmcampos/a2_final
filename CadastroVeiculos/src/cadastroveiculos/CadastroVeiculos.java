67ccfb316f8dabba0e4b4c84975d02eb821ada8f
Leodias-hue <72420287+Leodias-hue@users.noreply.github.com>
2021-06-08 19:50:46 -0300

    update 2

---
+++ CadastroVeiculos/src/cadastroveiculos/CadastroVeiculos.java
@@ -0,0 +1,14 @@
+
+package cadastroveiculos;
+
+import view.TelaPrincipal;
+
+public class CadastroVeiculos {
+
+    
+    public static void main(String[] args) {
+        TelaPrincipal principal = new TelaPrincipal ();
+        principal.setVisible(true);
+    }
+    
+}
