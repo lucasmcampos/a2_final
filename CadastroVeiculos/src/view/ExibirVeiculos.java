4d892cf066e961c7fd5282638ec9da3c99ed3852
lucasmcampos <lucasmcampos@outlook.com>
2021-06-17 20:36:40 -0300

    botao deletar adicionado

--- CadastroVeiculos/src/view/ExibirVeiculos.java
+++ CadastroVeiculos/src/view/ExibirVeiculos.java
@@ -59,6 +59,7 @@ public class ExibirVeiculos extends javax.swing.JDialog {
         btnSair = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         tblVeiculos = new javax.swing.JTable();
+        btnRemover = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -138,6 +139,13 @@ public class ExibirVeiculos extends javax.swing.JDialog {
         });
         jScrollPane1.setViewportView(tblVeiculos);
 
+        btnRemover.setText("Remover todos os veículos");
+        btnRemover.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRemoverActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -148,6 +156,8 @@ public class ExibirVeiculos extends javax.swing.JDialog {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnRemover)
+                .addGap(26, 26, 26)
                 .addComponent(btnSair)
                 .addContainerGap())
         );
@@ -157,7 +167,9 @@ public class ExibirVeiculos extends javax.swing.JDialog {
                 .addContainerGap()
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 464, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(btnSair)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnSair)
+                    .addComponent(btnRemover))
                 .addGap(6, 6, 6))
         );
 
@@ -168,6 +180,19 @@ public class ExibirVeiculos extends javax.swing.JDialog {
         // TODO add your handling code here:
         this.dispose(); //Apenas para fechar a janela
     }//GEN-LAST:event_btnSairActionPerformed
+
+    private void btnRemoverActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRemoverActionPerformed
+        // TODO add your handling code here:
+        ArrayList<Veiculo> veiculos = cv.retornarTodos();
+        
+        //Limpar JTable
+        
+        for(int i = 0; i < veiculos.size(); i++){
+            tblVeiculos.setValueAt("", i, 0);
+            tblVeiculos.setValueAt("", i, 1);
+            tblVeiculos.setValueAt("", i, 2);
+        }
+    }//GEN-LAST:event_btnRemoverActionPerformed
 
     /**
      * @param args the command line arguments
@@ -212,6 +237,7 @@ public class ExibirVeiculos extends javax.swing.JDialog {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnRemover;
     private javax.swing.JButton btnSair;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tblVeiculos;
